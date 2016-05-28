69622a2a0f8e71cba0fd13a1a76f20b2af9f6aa0
Duarte David <deltaduartedavid@gmail.com>
2016-05-28 18:48:58 +0200

    Set compatibility for java 6

--- src/main/java/fr/aresrpg/gradle/api/ApiPlugin.java
+++ src/main/java/fr/aresrpg/gradle/api/ApiPlugin.java
@@ -4,10 +4,12 @@ import fr.aresrpg.gradle.api.task.ApiJarTask;
 import fr.aresrpg.gradle.api.task.ApiTask;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
 import org.gradle.api.internal.artifacts.publish.ArchivePublishArtifact;
 import org.gradle.api.internal.component.SoftwareComponentInternal;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.JavaPluginConvention;
+import org.gradle.api.specs.Spec;
 import org.gradle.api.tasks.SourceSet;
 
 import java.io.File;
@@ -37,11 +39,24 @@ public class ApiPlugin implements Plugin<Project> {
 		ApiJarTask apiJarTask = project.getTasks().create(ApiJarTask.NAME , ApiJarTask.class);
 		apiJarTask.getDependsOn().add(apiTask);
 		apiJarTask.from(apiTask.getOutputFolder());
-		apiJarTask.setOnlyIf((e) -> apiTask.getState().getExecuted());
+		apiJarTask.setOnlyIf(new ApiJarTaskSpec(apiTask));
 		return apiJarTask;
 	}
 
 	private void configureComponent(Project project , ApiJarTask task){
 		project.getComponents().add(new ApiComponent((SoftwareComponentInternal) project.getComponents().getByName("java"), new ArchivePublishArtifact(task)));
+	}
+
+	private static class ApiJarTaskSpec implements Spec<Task>{
+		private ApiTask task;
+
+		private ApiJarTaskSpec(ApiTask task) {
+			this.task = task;
+		}
+
+		@Override
+		public boolean isSatisfiedBy(Task task) {
+			return this.task.getState().getExecuted();
+		}
 	}
 }
